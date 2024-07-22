0ee1ee697e9e5f309c85cf7ca9b8ef1a642607ba
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-07-22 10:00:17 +0200

    updated and simplify the code

--- unitconverter.java
+++ unitconverter.java
@@ -6,63 +6,32 @@ public class unitconverter {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter the unit you want to convert from:");
-        System.out.println("A - feet");
-        System.out.println("B - pounds");
-        System.out.println("C - fahrenheit");
+        System.out.println("A - feet to meters");
+        System.out.println("B - pounds to kilograms");
+        System.out.println("C - fahrenheit to celcius");
 
         String initialunit = scanner.nextLine().toUpperCase();
-
-        System.out.println("Enter the unit you want to convert to: ");
-        System.out.println("A - Meter");
-        System.out.println("B -   Kilograms");
-        System.out.println("C - To Celsius");
-        String finalunit = scanner.nextLine().toUpperCase();
-
         System.out.println("Enter the quantity to be converted: ");
         double quantity = scanner.nextDouble();
-
-        double result = conversion(initialunit, finalunit, quantity);
-
-        if (result != -1) {
-            System.out.printf("Converted value: %.2f %s%n", result, initialunit);
-        } else {
-            System.out.println("Invalid conversion units provided.");
-        }
-
+        
+        conversion(initialunit, quantity);
         scanner.close();
     }
 
-    public static double conversion(String initialunit, String finalunit, double quantity) {
+    public static double conversion(String initialunit, double quantity) {
         switch (initialunit) {
             case "A":
-                if (finalunit.equals("meters")) {
-                    return feetToMeters(quantity);
-                }
+                System.out.println(Math.round((quantity * 0.3048)) + " meters");
                 break;
             case "B":
-                if (finalunit.equals("kgs")) {
-                    return poundsToKgs(quantity);
-                }
+                System.out.println(Math.round((quantity * 0.453592)) + " Kg");
                 break;
             case "C":
-                if (finalunit.equals("C")) {
-                    return fahrenheitToCelsius(quantity);
-                }
+                System.out.println(Math.round(((quantity - 32)) * 5 / 9) + " Celcius");
                 break;
         }
         return -1; 
     }
 
-    public static double feetToMeters(double feet) {
-        return feet * 0.3048;
-    }
-
-    public static double poundsToKgs(double pounds) {
-        return pounds * 0.453592;
-    }
-
-    public static double fahrenheitToCelsius(double fahrenheit) {
-        return (fahrenheit - 32) * 5 / 9;
-    }
 }
 
